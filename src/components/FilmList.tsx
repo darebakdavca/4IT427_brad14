9d4f8e24bea911273ee5b3c6e1957c37925678dc
David Brablc <129985521+darebakdavca@users.noreply.github.com>
2026-05-20 15:50:14 +0200

    fix: update FilmList component to handle empty films and rename toggle function

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -6,16 +6,14 @@ interface FilmListProps {
 }
 
 export function FilmList({ films }: FilmListProps) {
-  if (!films) return (<p>Žádné dostupné filmy</p>)
-
-  const toggleWatched = (title: Film['title']) => {
+  const handleToggleWatched = (title: Film['title']) => {
     console.log(`Toggle watch clicked on film ${title}`)
   }
 
   return (
     <div>
       {films?.map((film, index) => (
-        <FilmCard key={index} {...film} onToggleWatched={toggleWatched} />
+        <FilmCard key={index} {...film} onToggleWatched={handleToggleWatched} />
       ))}
     </div>
   )
