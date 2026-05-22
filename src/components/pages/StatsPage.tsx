b4519cd93d2191d097abbbeea616f2758f45242f
David Brablc <129985521+darebakdavca@users.noreply.github.com>
2026-05-22 14:02:20 +0200

    refactor: remove unused useWatchList import from StatsPage component

--- src/components/pages/StatsPage.tsx
+++ src/components/pages/StatsPage.tsx
@@ -1,7 +1,4 @@
-import { useWatchList } from "@/contexts/WatchListContext";
-
 export function StatsPage() {
-  const { films } = useWatchList();
   return (
     <div>
       <p>Here would be some fun stats...</p>
